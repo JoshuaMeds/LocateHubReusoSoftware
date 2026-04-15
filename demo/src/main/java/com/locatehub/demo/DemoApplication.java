391f266af3999aa8cee200742ec261d0433fa979
MuriloBertelli <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>
2026-04-15 20:55:50 -0300

    feat/backend/dashBoards-Money/implement-models-main-resolution

--- demo/src/main/java/com/locatehub/demo/DemoApplication.java
+++ demo/src/main/java/com/locatehub/demo/DemoApplication.java
@@ -3,11 +3,21 @@ package com.locatehub.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.locatehub.demo.dashboard.DashboardFinanceiroTemplate;
+import com.locatehub.demo.dashboard.DashboardLocador;
+import com.locatehub.demo.dashboard.DashboardLocatario;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		DashboardFinanceiroTemplate painelLocatario = new DashboardLocatario();
+		painelLocatario.gerarDashboard(); // Gera a tela de despesas
+
+		DashboardFinanceiroTemplate painelLocador = new DashboardLocador();
+		painelLocador.gerarDashboard(); // Gera a tela de ganhos
 	}
 
 }
